aa97123d1081c95d3bc8bef5d6bcff45aafea3c1
Alok sinha <89570069+AlokPng@users.noreply.github.com>
2023-02-18 20:39:36 +0530

    Add files via upload

---
+++ tut/projects/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from werkzeug import Request
+from .models import Project
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+# Create your views here.
+def projects(request):
+    projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+    
+def projects(rquest):     
+    context={'projects': projectsList}
+    return render(Request, 'projects/index.html', context)
+def project(request,pk):
+    return render(request, 'projects/single-projects.html')
